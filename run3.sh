9349b71fbc9e91948f42afd22b8fce37ac8f0224
Jens Harbott <j.harbott@x-ion.de>
2018-08-27 14:59:46 +0000

    Fix run3.sh to use the bin directory

--- run3.sh
+++ run3.sh
@@ -1,11 +1,11 @@
 for i in 1 2 3; do
  date
- ./create_buckets 
- ./test_put
- ./test_get_rand
- ./test_copy
- ./test_delete_copy
- ./test_delete
- ./delete_buckets
+ bin/create_buckets
+ bin/test_put
+ bin/test_get_rand
+ bin/test_copy
+ bin/test_delete_copy
+ bin/test_delete
+ bin/delete_buckets
 done
 date
